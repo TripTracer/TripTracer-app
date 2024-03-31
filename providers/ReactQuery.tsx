4cff38276a6aa2a0da20acc76990b7c3c6b8f866
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-03-31 21:52:40 +0330

    feat: working on dynamic dark mode.

---
+++ providers/ReactQuery.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+// Create a client
+const queryClient = new QueryClient();
+
+export default function QueryProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
