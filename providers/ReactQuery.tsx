36df8716bf1e65dc6600a037fc3004362f9f5faf
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-04-06 13:39:13 +0330

    feat: added font files

--- providers/ReactQuery.tsx
+++ providers/ReactQuery.tsx
@@ -1,6 +1,5 @@
 import { ReactNode } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 // Create a client
 const queryClient = new QueryClient();
