0128a25f2bcd27ef78a70aaed58e725568b5489c
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-04-07 16:57:11 +0330

    fix: working on dynamic dark mode

--- components/settingsScreen/ThemeChanger.tsx
+++ components/settingsScreen/ThemeChanger.tsx
@@ -1,16 +1,60 @@
 import { useColorScheme, Appearance } from 'react-native';
-import { Switch, useTheme } from 'react-native-paper';
+import { SegmentedButtons, useTheme } from 'react-native-paper';
 import { PreferencesContext } from '../../providers/PreferencesContext';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { use } from 'i18next';
 
 const ThemeChanger = () => {
   const colorScheme2 = Appearance.getColorScheme();
-  const colorScheme = useColorScheme();
+  const colorScheme3 = useColorScheme();
   const theme = useTheme();
-  const { toggleTheme, isThemeDark } = useContext(PreferencesContext);
+  const { colorScheme, setColorScheme } = useContext(PreferencesContext);
+  const [value, setValue] = useState(colorScheme);
 
+  const onValueChange = async (value: string) => {
+    setColorScheme(value);
+    setValue(value);
+    try {
+      await AsyncStorage.setItem('colorScheme', value);
+    } catch (error) {
+      console.error('Error saving theme to AsyncStorage:', error);
+    }
+  };
+
+  const getCurrentTheme = async () => {
+    try {
+      const storedColorScheme = await AsyncStorage.getItem('colorScheme');
+      if (storedColorScheme == null || storedColorScheme === 'system') {
+        setColorScheme('system');
+        setValue('system');
+      } else {
+        setColorScheme(storedColorScheme);
+        setValue(storedColorScheme);
+      }
+    } catch (error) {
+      console.error('Error loading theme from AsyncStorage:', error);
+    }
+  };
+  useEffect(() => {
+    getCurrentTheme();
+  });
   return (
-    <Switch color={'red'} value={isThemeDark} onValueChange={toggleTheme} />
+    <SegmentedButtons
+      value={value}
+      onValueChange={onValueChange}
+      buttons={[
+        {
+          value: 'light',
+          label: 'Light',
+        },
+        {
+          value: 'dark',
+          label: 'Dark',
+        },
+        { value: 'system', label: 'System Default' },
+      ]}
+    />
   );
 };
 
