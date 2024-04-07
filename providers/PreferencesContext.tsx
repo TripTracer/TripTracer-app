560dff3cfa12065f7dbb3faf9c1222e59ab432d2
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-04-07 17:23:05 +0330

    feat: dynamic language system added.

--- providers/PreferencesContext.tsx
+++
@@ -1,6 +0,0 @@
-import { createContext } from 'react';
-
-export const PreferencesContext = createContext({
-  colorScheme: 'system',
-  setColorScheme: (colorScheme: string) => {},
-});
---
+++ utils/PreferencesContext.tsx
@@ -0,0 +1,11 @@
+import { createContext } from 'react';
+
+export const ColorSchemeContext = createContext({
+  colorScheme: 'system',
+  setColorScheme: (colorScheme: string) => {},
+});
+
+export const LanguageContext = createContext({
+  language: 'en',
+  setLanguage: (language: string) => {},
+});
