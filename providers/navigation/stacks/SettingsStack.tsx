ed7af29371d607ebda424d60b5beb07614ef0d8c
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-04-09 19:07:18 +0330

    feat: hide header for some specific screens

--- providers/navigation/stacks/SettingsStack.tsx
+++ providers/navigation/stacks/SettingsStack.tsx
@@ -1,5 +1,7 @@
+import { getHeaderTitle } from '@react-navigation/elements';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import TopBar from '../../../components/appBar/TopBar';
 import DetailsScreen from '../../../screens/Details';
 import SettingsScreen from '../../../screens/Settings';
 import { SettingsStackParamList } from '../../../utils/types';
@@ -8,8 +10,18 @@ const SettingsStack = createNativeStackNavigator<SettingsStackParamList>();
 
 function SettingsStackScreen() {
   return (
-    <SettingsStack.Navigator>
-      <SettingsStack.Screen name='Settings' component={SettingsScreen} />
+    <SettingsStack.Navigator
+      screenOptions={{
+        header: ({ options, route }) => (
+          <TopBar title={getHeaderTitle(options, route.name)} />
+        ),
+      }}
+    >
+      <SettingsStack.Screen
+        name='Settings'
+        component={SettingsScreen}
+        options={{ headerShown: false }}
+      />
       <SettingsStack.Screen name='Details' component={DetailsScreen} />
     </SettingsStack.Navigator>
   );
