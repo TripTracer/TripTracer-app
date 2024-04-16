b2a737999902a0000fe93142d18912aa474b0067
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-04-16 17:36:07 +0330

    feat: the QueryBuilder test added.

---
+++ components/queryBuilder/QueryBuilder.test.tsx
@@ -0,0 +1,58 @@
+import { render, waitFor } from '@testing-library/react-native';
+import QueryBuilderComponent from './QueryBuilder';
+import '@testing-library/jest-dom';
+
+describe('Query Builder', () => {
+  // Renders QueryBuilder component with provided fields and query props.
+  it('should render QueryBuilder component with provided fields and query props', () => {
+    // Arrange
+    const fields = [
+      { name: 'field1', label: 'Field 1' },
+      { name: 'field2', label: 'Field 2' },
+    ];
+    const query = { id: '1', rules: [], combinator: 'and' };
+    const setQuery = jest.fn();
+
+    // Act
+    render(
+      <QueryBuilderComponent
+        fields={fields}
+        query={query}
+        setQuery={setQuery}
+      />,
+    );
+
+    waitFor(() => {
+      // Assert
+      expect(QueryBuilderComponent).toHaveBeenCalledWith({
+        fields,
+        query,
+        onQueryChange: setQuery,
+      });
+    });
+  });
+  it('should render QueryBuilder component with no fields when fields array is empty', () => {
+    // Arrange
+    const fields = [];
+    const query = { id: '1', rules: [], combinator: 'and' };
+    const setQuery = jest.fn();
+
+    // Act
+    render(
+      <QueryBuilderComponent
+        fields={fields}
+        query={query}
+        setQuery={setQuery}
+      />,
+    );
+
+    waitFor(() => {
+      // Assert
+      expect(QueryBuilderComponent).toHaveBeenCalledWith({
+        fields,
+        query,
+        onQueryChange: setQuery,
+      });
+    });
+  });
+});
