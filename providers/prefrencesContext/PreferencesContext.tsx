1a0493790a09a9bd0c7f56ac8a20723ed8cfc726
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-04-18 13:38:29 +0330

    fix: fixing file conflicts

--- providers/prefrencesContext/PreferencesContext.tsx
+++ providers/prefrencesContext/PreferencesContext.tsx
@@ -1,83 +1,11 @@
 import { createContext } from 'react';
 
-export const PreferencesContext = createContext({
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD:providers/prefrencesContext/PreferencesContext.tsx
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD:providers/prefrencesContext/PreferencesContext.tsx
-<<<<<<< HEAD
-=======
->>>>>>> 1376e07 (feat: theme unit tests added)
-=======
->>>>>>> b5a2cfb (feat: theme unit tests added)
-=======
-=======
->>>>>>> 1376e07 (feat: theme unit tests added)
->>>>>>> 9789212 (feat: theme unit tests added)
+export const ColorSchemeContext = createContext({
   colorScheme: 'system',
   setColorScheme: (colorScheme: string) => {},
-=======
-  toggleTheme: () => {},
-  isThemeDark: false,
->>>>>>> 07c3b61 (feat: theme unit tests added)
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 64a70fc (fix: working on dynamic dark mode)
-=======
->>>>>>> 9789212 (feat: theme unit tests added)
-=======
-  colorScheme: 'system',
-  setColorScheme: (colorScheme: string) => {},
->>>>>>> ce7c289 (fix: working on dynamic dark mode):providers/PreferencesContext.tsx
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 9851c96 (feat: theme unit tests added)
-=======
-  toggleTheme: () => {},
-  isThemeDark: false,
->>>>>>> f46e10f (feat: theme unit tests added)
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 7adf7f1 (fix: working on dynamic dark mode)
-=======
-  colorScheme: 'system',
-  setColorScheme: (colorScheme: string) => {},
->>>>>>> f5d01a8 (fix: working on dynamic dark mode)
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 1376e07 (feat: theme unit tests added)
-=======
-  toggleTheme: () => {},
-  isThemeDark: false,
->>>>>>> 00e1a5a (feat: theme unit tests added)
-=======
-  colorScheme: 'system',
-  setColorScheme: (colorScheme: string) => {},
->>>>>>> 2219c58 (fix: working on dynamic dark mode)
-=======
->>>>>>> b5a2cfb (feat: theme unit tests added)
-=======
->>>>>>> 64a70fc (fix: working on dynamic dark mode)
-=======
->>>>>>> 9851c96 (feat: theme unit tests added)
-=======
->>>>>>> 7adf7f1 (fix: working on dynamic dark mode)
-=======
-=======
->>>>>>> 1376e07 (feat: theme unit tests added)
->>>>>>> 9789212 (feat: theme unit tests added)
+});
+
+export const LanguageContext = createContext({
+  language: 'en',
+  setLanguage: (language: string) => {},
 });
