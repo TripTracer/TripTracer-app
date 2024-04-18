6b67c8dc4fd21a0d34c9addb27f2d1e1aae80135
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-04-18 12:43:22 +0330

    fix: fixed package.json file conflicts

--- components/queryBuilder/QueryBuilder.test.tsx
+++ components/queryBuilder/QueryBuilder.test.tsx
@@ -1,9 +1,13 @@
 import { render, waitFor } from '@testing-library/react-native';
 import QueryBuilderComponent from './QueryBuilder';
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 import '@testing-library/jest-dom';
 >>>>>>> 4a79951 (feat: the QueryBuilder test added.)
+=======
+import '@testing-library/jest-dom';
+>>>>>>> 4f95cee (feat: the QueryBuilder test added.)
 
 describe('Query Builder', () => {
   // Renders QueryBuilder component with provided fields and query props.
