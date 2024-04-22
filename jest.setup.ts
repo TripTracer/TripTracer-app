2a811b1e809172fe99cf30b1a5fe53d2770d195d
sepehrsohrabii <sepehr0sohrabi@gmail.com>
2024-04-22 18:44:14 +0330

    fix: fixed all jest failure errors

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,4 +1,6 @@
 import '@testing-library/react-native/extend-expect';
+// include this line for mocking react-native-gesture-handler
+import 'react-native-gesture-handler/jestSetup';
 
 const matchers = require('jest-extended');
 expect.extend(matchers);
@@ -10,3 +12,17 @@ afterEach(() => {
 jest.mock('@react-native-async-storage/async-storage', () =>
   require('@react-native-async-storage/async-storage/jest/async-storage-mock'),
 );
+
+// include this section and the NativeAnimatedHelper section for mocking react-native-reanimated
+jest.mock('react-native-reanimated', () => {
+  const Reanimated = require('react-native-reanimated/mock');
+
+  // The mock for `call` immediately calls the callback which is incorrect
+  // So we override it with a no-op
+  Reanimated.default.call = () => {};
+
+  return Reanimated;
+});
+
+// Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
